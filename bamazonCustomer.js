7bbf7f91112b817f54c8650974625fff91db0564
Chad Cook <chadcookcoding@gmail.com>
2018-12-12 20:42:23 -0500

    finished functions for fully working customer portion of homework

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -23,8 +23,56 @@ const customerOrder = () => {
     // console.log(res[0].item_id.toString().padEnd(4, ' '))
     // console.log(typeof res[0].price)
     for ( let i in res ) {
-      console.log(`ID: ${ res[i].item_id.toString().padEnd(3, ' ') } Item: ${ res[i].product_name.padEnd(25, ' ') } Price: ${ res[i].price }\n${ line.padEnd(55, '-') }`)
-    }
-    connection.end()
+      let id = res[i].item_id.toString().padEnd(3, ' ')
+      let itemName = res[i].product_name.padEnd(25, ' ')
+      let price = res[i].price.toFixed(2).toString().padStart(8, ' ')
+      console.log(`ID: ${ id } Item: ${ itemName } Price: $${ price }\n${ line.padEnd(56, '-') }`)
+    }  
+    inquirer
+      .prompt([
+        {
+          name: 'itemChoice',
+          type: 'input',
+          message: 'What is the ID of the item you wish to purchase?'
+        },
+        {
+          name: 'quantity',
+          type: 'input',
+          message: 'How many would you like?'
+        }
+      ])
+      .then(function(answer) {
+        let chosenItem
+        // console.log(res[0].item_id)
+        // console.log(answer.itemChoice)
+        for ( let i in res ) {
+          if ( res[i].item_id === parseInt(answer.itemChoice) ) {
+            chosenItem = res[i]
+          }
+        }
+        // console.log(chosenItem)
+
+        if ( chosenItem.stock_quantity > answer.quantity ) {
+          // update database with purchase and show customer total cost of purchase
+          let newStockQuantity = chosenItem.stock_quantity - answer.quantity
+          connection.query(
+            "UPDATE products SET ? WHERE ?",
+            [
+              { stock_quantity: newStockQuantity },
+              { item_id: chosenItem.item_id }
+            ],
+            function(err, res ) {
+              if ( err ) throw err
+              console.log(`\nYou successfully purchased ${ answer.quantity } ${ chosenItem.product_name } for a total of $${ (answer.quantity * chosenItem.price).toFixed(2) }\n`)
+            }
+          )
+          connection.end()
+
+        } else {
+          console.log(`Insufficient stock quantity!`)
+          connection.end()
+        }
+      })
   })
+
 }
