b200c1e26696bddfc56370973ce544f05137e33c
Chad Cook <chadcookcoding@gmail.com>
2018-12-12 19:13:11 -0500

    created connect function and added a loop to print all items to console

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -10,3 +10,21 @@ const connection = mysql.createConnection({
   database: 'bamazon'
 })
 
+connection.connect(function( err ) {
+  if ( err ) throw err
+  console.log(`connected as id ${ connection.threadId }`)
+  customerOrder()
+})
+
+const customerOrder = () => {
+  connection.query('SELECT * FROM products', function( err, res ) {
+    if ( err ) throw err
+    let line = `-----------------------------------------------`
+    // console.log(res[0].item_id.toString().padEnd(4, ' '))
+    // console.log(typeof res[0].price)
+    for ( let i in res ) {
+      console.log(`ID: ${ res[i].item_id.toString().padEnd(3, ' ') } Item: ${ res[i].product_name.padEnd(25, ' ') } Price: ${ res[i].price }\n${ line.padEnd(55, '-') }`)
+    }
+    connection.end()
+  })
+}
